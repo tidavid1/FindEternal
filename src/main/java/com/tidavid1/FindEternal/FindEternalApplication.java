656c3db80ae5ff76d0d596ddf4ba8050299b39a1
tidavid1 <hello.tidavid@gmail.com>
2023-08-21 16:34:16 +0900

    INIT: Initial Commit

---
+++ src/main/java/com/tidavid1/FindEternal/FindEternalApplication.java
@@ -0,0 +1,13 @@
+package com.tidavid1.FindEternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindEternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindEternalApplication.class, args);
+	}
+
+}
