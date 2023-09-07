cb24b88f6a7ce8f4ef528c25bf1fb8b964d7d366
tidavid1 <hello.tidavid@gmail.com>
2023-09-07 18:54:47 +0900

    FIX: WebClientProvider 작동 위치를 Service 로 변경

--- src/main/java/com/tidavid1/FindEternal/domain/player/service/PlayerService.java
+++ src/main/java/com/tidavid1/FindEternal/domain/player/service/PlayerService.java
@@ -1,9 +1,11 @@
 package com.tidavid1.FindEternal.domain.player.service;
 
+import com.tidavid1.FindEternal.common.config.WebClientProvider;
 import com.tidavid1.FindEternal.domain.player.entity.Player;
 import com.tidavid1.FindEternal.domain.player.exception.CPlayerNotFoundException;
 import com.tidavid1.FindEternal.domain.player.repository.PlayerRepository;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,14 +15,28 @@ import java.util.Optional;
 @Service
 public class PlayerService {
     private final PlayerRepository playerRepository;
+    private final WebClientProvider webClientProvider;
 
     @Transactional
-    public Player findByUserNum(Integer userNum){
-        return playerRepository.findByUserNum(userNum).orElseThrow(CPlayerNotFoundException::new);
+    public Player findByNickname(String nickname){
+        return playerRepository.findByNickname(nickname).orElseGet(()->{
+            JSONObject result = webClientProvider.requestNicknameAPI(nickname);
+            if(!(result.getInt("code")==200)){
+                throw new CPlayerNotFoundException();
+            }
+            result = result.getJSONObject("user");
+            JSONObject finalResult = result;
+            Player temp = playerRepository.findByUserNum(result.getInt("userNum")).orElseGet(()->{
+                Player newPlayer = Player.builder().userNum(finalResult.getInt("userNum")).nickname(nickname).build();
+                addUser(newPlayer);
+                return newPlayer;}
+            );
+            if (!temp.getNickname().equals(nickname)){
+                temp.updateNickname(nickname);
+            }
+            return temp;
+        });
     }
-
-    @Transactional
-    public Optional<Player> findByNickname(String nickname){return playerRepository.findByNickname(nickname);}
 
     @Transactional
     public void addUser(Player player){
